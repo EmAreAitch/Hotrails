6e93a9191c3212c218f4e5d2cb6031d2c3b61a87
emareaitch <dev.emareaitch@gmail.com>
2024-02-07 22:54:52 +0530

    Completed entire project

---
+++ app/models/quote.rb
@@ -0,0 +1,13 @@
+class Quote < ApplicationRecord
+  belongs_to :company
+  has_many :line_item_dates, dependent: :destroy  
+  validates :name, presence: true
+  has_many :line_items, through: :line_item_dates
+
+  scope :ordered, -> { order(updated_at: :desc)}
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
+end
